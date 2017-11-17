ea1e4ec1d97a01e762f4fe97281bbc53960d9509
Jihun Kim <devgrapher@ridi.com>
2017-11-17 19:00:10 +0900

    Fix ecs-cli version

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,7 @@ if ! [ -x "$(command -v ecs-cli)" ]; then
     else
        PLATFORM='linux'
     fi
-    curl -o ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-${PLATFORM}-amd64-latest && chmod +x ecs-cli
+    curl -o ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-${PLATFORM}-amd64-v1.0.0 && chmod +x ecs-cli
     ./ecs-cli compose up -c ${AWS_ECS_CLUSTER}
 else
     ecs-cli compose up -c ${AWS_ECS_CLUSTER}
